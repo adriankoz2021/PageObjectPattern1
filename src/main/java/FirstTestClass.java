939fcdfa73caa4dd9b04fc2712bea9072a068f69
adriankoz2021 <akozlowski@astek.pl>
2021-05-21 22:06:18 +0200

    ssssfdf

--- src/main/java/FirstTestClass.java
+++ src/main/java/FirstTestClass.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -13,49 +16,41 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstTestClass {
     WebDriver driver;
+
     @BeforeTest
-    void beforTest(){
+    void beforTest() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Konsultant\\IdeaProjects\\SeleniumTestng\\src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    void firstTest() {
+    public void ExplicitWaitTest() {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
         driver.navigate().to("http://the-internet.herokuapp.com/");
-        By checkboxes = By.partialLinkText("Checkboxes");
-        WebElement checkboxesLink = driver.findElement(checkboxes);
-       checkboxesLink.click();
-        By checked = By.xpath("//input[1]");
-        WebElement checked1 = driver.findElement(checked);
-        checked1.click();
-        boolean boo =  checked1.isSelected();
-        Assert.assertEquals(boo=true,"Pierwszy checkbox został zaznaczony");
-    }
 
-    @Test
-    void secondTest(){
-        driver.navigate().to("http://the-internet.herokuapp.com/");
-        By checkboxes = By.partialLinkText("Checkboxes");
-        WebElement checkboxesLink = driver.findElement(checkboxes);
-        checkboxesLink.click();
-        By checked2 = By.xpath("//input[2]");
-        WebElement checked3 = driver.findElement(checked2);
-        boolean boo2 = checked3.isSelected();
-        if (boo2=true){System.out.println("Drugi checkbox był zaznaczony");
-        }
-        else{
-            System.out.println("Drugi checkbox  nie był zaznaczony");
-        }
-        checked3.click();
-        boolean boo3 = checked3.isSelected();
-        Assert.assertEquals(boo3=true,"Drugi checkbox został odznaczony");
+        WebElement element = driver.findElement(By.xpath("//h1[text()='Welcome to the-internet']"));
+
+        webDriverWait.until(ExpectedConditions.visibilityOf(element));
+        WebElement elementAdd = driver.findElement(By.xpath("//ul/li[2]/a"));
+        elementAdd.click();
+        WebElement addHeader = driver.findElement(By.xpath("//h3[text()='Add/Remove Elements']"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(addHeader));
+        WebElement addElementClick = driver.findElement(By.xpath("//div[@id='content']/div/button"));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(addElementClick));
+        addElementClick.click();
+        WebElement delete = driver.findElement(By.xpath("//div[@id=\"elements\"]/button"));
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(delete));
+        delete.click();
+        webDriverWait.until(ExpectedConditions.invisibilityOf(delete));
 
     }
-        @AfterTest
-                void afterTest(){
-            driver.close();
-            driver.quit();
-        }
+
+    @AfterTest
+    void afterTest() {
+        driver.close();
+        driver.quit();
     }
+}
+
 
